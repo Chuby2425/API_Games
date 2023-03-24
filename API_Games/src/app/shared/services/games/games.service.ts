7987a32e3615eaca9a5cda7e56cfc47786bfa624
Jesús De la Fuente <jesusdelafureyesjdlfr@gmail.com>
2023-03-24 16:01:12 -0600

    Cambios
    Se consumió el api y se pintan los datos en home

--- API_Games/src/app/shared/services/games/games.service.ts
+++ API_Games/src/app/shared/services/games/games.service.ts
@@ -9,6 +9,6 @@ export class GamesService {
   constructor(private http: HttpClient) { }
 
   getGames(){
-    return this.http.get('https://api.rawg.io/api/games?key=d4e382cfc6414bc8ae6ab2afc166b36d');
+    return this.http.get('https://api.rawg.io/api/games?key=d4e382cfc6414bc8ae6ab2afc166b36d&page=1');
   }
 }
