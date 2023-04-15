6ea59e569f22f26df8262006ced0892688744ee8
Jesús De la Fuente <jesusdelafureyesjdlfr@gmail.com>
2023-04-14 22:23:36 -0600

    Cambios

--- API_Games/src/app/action/components/action-page/action-page.component.ts
+++ API_Games/src/app/action/components/action-page/action-page.component.ts
@@ -1,12 +1,13 @@
 import { Component , OnInit  } from '@angular/core';
 import { GenresService } from 'src/app/shared/services/genres/genres.service';
 
-
-
 interface ApiResponse {
   results: any[];
 }
 
+interface GameApiResponse {
+  results: any[];
+}
 
 @Component({
   selector: 'app-action-page',
@@ -15,15 +16,22 @@ interface ApiResponse {
 })
 export class ActionPageComponent implements OnInit {
 
+  genre: any;
+  games: any[] = [];
 
-  genres: any[] = [];
-
-  constructor(private genresService: GenresService){ }
+  constructor(private genresService: GenresService) { }
 
   ngOnInit(): void {
     this.genresService.getGenres()
-    .subscribe((data: ApiResponse) => {
-      this.genres = data.results; });
-      }
-    }
+      .subscribe((data: ApiResponse) => {
+        console.log(data)
+        this.genre = data.results[0]; // Obtener el primer género
+        this.genresService.getGames(this.genre.id) // Obtener los juegos del género
+          .subscribe((data: GameApiResponse) => {
+            console.log(data)
+            this.games = data.results.filter(game => game.genre === this.genre.name);
+          });
+      });
+  }
+}
 
--- API_Games/src/app/shared/services/genres/genres.service.ts
+++ API_Games/src/app/shared/services/genres/genres.service.ts
@@ -7,16 +7,25 @@ interface ApiResponse {
   results: any[];
 }
 
+interface GameApiResponse {
+  results: any[];
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class GenresService {
 
+  private gamesUrl = 'https://api.rawg.io/api/games?key=d4e382cfc6414bc8ae6ab2afc166b36d';
+
   constructor(private http: HttpClient) { }
 
-
   getGenres(): Observable<ApiResponse>{
-  return this.http.get<ApiResponse>('https://api.rawg.io/api/genres?key=d4e382cfc6414bc8ae6ab2afc166b36d&page=1');
+    return this.http.get<ApiResponse>('https://api.rawg.io/api/genres?key=d4e382cfc6414bc8ae6ab2afc166b36d');
+  }
+
+  getGames(id: number) {
+    const url = `${this.gamesUrl}?genres=${id}`;
+    return this.http.get<GameApiResponse>(url);
   }
 }
