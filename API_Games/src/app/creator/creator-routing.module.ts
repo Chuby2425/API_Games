177827697ef005e054d26ab410c9998f41b7bef0
Jason Delgado Aguilar <jeison_chuby0998@outlook.com>
2023-04-28 00:41:08 -0600

    Clean
    Clean creator

--- API_Games/src/app/creator/creator-routing.module.ts
+++ API_Games/src/app/creator/creator-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreatorPageComponent } from './components/creator-page/creator-page.component';
-import { CreatorDetailComponent } from './components/creator-detail/creator-detail.component';
 
 const routes: Routes = [
   { path: '', component: CreatorPageComponent },
-  { path: ':id', component: CreatorDetailComponent }
 ];
 
 @NgModule({
--- API_Games/src/app/creator/creator.module.ts
+++ API_Games/src/app/creator/creator.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreatorRoutingModule } from './creator-routing.module';
 import { CreatorPageComponent } from './components/creator-page/creator-page.component';
-import { CreatorDetailComponent } from './components/creator-detail/creator-detail.component';
+
 
 
 @NgModule({
   declarations: [
     CreatorPageComponent,
-    CreatorDetailComponent
   ],
   imports: [
     CommonModule,
